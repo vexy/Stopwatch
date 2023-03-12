1604a6001e36adbeb379937b74c6594bd0165a7e
Veljko Tekelerović <veljko.tekelerovic@gmail.com>
2023-03-12 21:24:37 +0100

    Adjusted acceess level for outside callers

--- Sources/Stopwatch/Stopwatch.swift
+++ Sources/Stopwatch/Stopwatch.swift
@@ -7,10 +7,10 @@ public struct Stopwatch {
     private static var startDate: Date = Date()
     
     /// Marks the beginning of the measuring interval
-    static func begin() { startDate = Date () }
+    public static func begin() { startDate = Date () }
     
     /// Returns a `formatted string` containing number of seconds since last `begin()` call
-    static var elapsed: String {
+    public static var elapsed: String {
         let elapsedTI = Date().timeIntervalSince(startDate)
         if elapsedTI < 1 {
             return String(format: "%.4f", elapsedTI)
